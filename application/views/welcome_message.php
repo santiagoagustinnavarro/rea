63e0ff4727c7ec58e8f10b472b8d436990108f48
snavarro017 <santiagonavarro@outlook.com.ar>
2018-10-01 12:35:51 -0300

    prueba button

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -85,7 +85,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
-
+<button class="btn btn-info">HOla</button>
 
 </body>
 </html>
