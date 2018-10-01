acce32451a38e9ed4c74fda39dfa8d8dc6b35e1e
snavarro017 <santiagonavarro@outlook.com.ar>
2018-10-01 13:50:21 -0300

    cierre de etiqueta style

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" />
 	<script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js" ></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery.min.js" ></script>
-	<style type="text/css">
+	<style type="text/css"></style>
 </head>
 <body>
 <div id="container">
