878c5661ab6ff84e58b4954914322c63adc41e73
ElyB06 <ely-06nqn@hotmail.com>
2018-10-03 12:03:44 -0300

    controladores

--- application/controllers/usuario.php
+++ application/controllers/usuario.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Usuario extends CI_Controller {
-    function login(){
+    function traerDatos(){
         $this->load->model('Usuario_model');
-        $res=$this->Usuario_model->login();
-        $this->load->view('testUsuario',["logeo"=>$res]);
+        $data['usuarios']=$this->Usuario_model->getData();
+        $this->load->view('testUsuario',$data);
     }
 }
 
