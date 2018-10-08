10e149ef9d411f504018ebdcda17161da8b4aa9c
ElyB06 <ely-06nqn@hotmail.com>
2018-10-08 14:07:30 -0300

    Ultimo

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -10,18 +10,18 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg fixed-top" id="menu">
-        <div class="collapse navbar-collapse">
+        <div class="navbar-collapse">
 			<ul class="nav navbar-nav">
-				<li class="nav-item"><a href="#" class="nav-link" alt="Logo Rea"><img src="assets/imagenes/logo2.png"></a></li>
+				<li class="nav-item"><a href="#" class="nav-link"><img src="assets/imagenes/logo2.png"></a></li>
 			</ul>
-            <ul class="nav navbar-nav">
+            <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item"><a href="#" class="nav-link text-secondary" alt="inicio">Inicio</a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-secondary" alt="area">Área</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-secondary" alt="contacto">Contacto</a></li>
+				<li class="nav-item"><a href="#" class="nav-link text-secondary" alt="contacto">Contacto</a></li>
             </ul>
             <ul class="nav navbar-nav ml-auto">
-                <li "nav navbar-nav">
-                    <button type="button" data-toggle="dropdown" class="btn btn-outline-secondary">Registrarse</button>
+				<li class="nav-item">
+                    <a href="#"><button type="button" class="btn btn-outline-secondary">Registrarse</button></a>
                 </li>
             </ul>
   	  </div>
--- application/views/inicio/registrarse.php
+++ application/views/inicio/registrarse.php
@@ -1,1 +1,33 @@
-<h1>Formulario de Registro</h1>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Registro de Usuario</title>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" />
+		<script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery.min.js" ></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css"/>
+  		<script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js" ></script>
+	</head>
+	<body>
+<div class="container">
+<h1 class="text-secondary">Registro Usuario</h1>
+  <form method="post" action="ej.php">
+    <div class="form-group">
+		<label for="nombre" class="text-secondary">Nombre</label>
+      <input type="text" class="form-control" id="nombre" placeholder="Nombre" name="nombre">
+			<label for="apellido" class="text-secondary">Apellido</label>
+      <input type="text" class="form-control" id="apellido" placeholder="Apellido" name="apellido">
+      <label for="email" class="text-secondary">Correo Electronico</label>
+      <input type="email" class="form-control" id="email" placeholder="ejemplo@..." name="email">
+			<label for="user" class="text-secondary">Nombre del Usuario</label>
+      <input type="text" class="form-control" id="user" placeholder="Nombre Usuario" name="user">
+    </div>
+    <div class="form-group">
+      <label for="clave" class="text-secondary">Contraseña</label>
+      <input type="password" class="form-control" id="clave" placeholder="Ingrese contraseña" name="clave">
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+  </form>
+</div>
+	</body>
+</html>
