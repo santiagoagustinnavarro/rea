1e91c56faa2b30bae849bd3e47c3e37b6fd850ae
ElyB06 <ely-06nqn@hotmail.com>
2018-10-22 21:35:45 -0300

    modificacion del footer

--- application/views/inicio/area.php
+++ application/views/inicio/area.php
@@ -1,27 +1,25 @@
 <h1>Tipos de Temas a Enseñar</h1>
-
-
 <!-- Pagination -->
 <ul class="pagination justify-content-center">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-          </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">1</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">3</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-          </a>
-        </li>
-      </ul>
+  <li class="page-item">
+    <a class="page-link" href="#" aria-label="Anterior">
+      <span aria-hidden="true">&laquo;</span>
+      <span class="sr-only">Anterior</span>
+    </a>
+  </li>
+  <li class="page-item">
+    <a class="page-link" href="#">1</a>
+  </li>
+  <li class="page-item">
+    <a class="page-link" href="#">2</a>
+  </li>
+  <li class="page-item">
+    <a class="page-link" href="#">3</a>
+  </li>
+  <li class="page-item">
+    <a class="page-link" href="#" aria-label="Siguiente">
+			<span aria-hidden="true">&raquo;</span>
+      <span class="sr-only">Siguiente</span>
+		</a>
+  </li>
+</ul>
