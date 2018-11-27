19da3a4adce46c6c3905f2bbe628bd0d7aa9cf8e
snavarro017 <santiagonavarro@outlook.com.ar>
2018-11-27 17:28:22 -0300

    filtrado de tablas

--- application/views/recurso/index.php
+++ application/views/recurso/index.php
@@ -5,7 +5,21 @@
 <script type="text/javascript">
 	$(document).ready(function () {
     $("#busqueda").keyup(function () {
-        var filtro = $("#busqueda").val();
+		var filtro = $("#busqueda").val();
+		if( jQuery(this).val() != ""){
+        jQuery("#tabla tbody>tr").hide();
+        jQuery("#tabla td:contiene-palabra('" + jQuery(this).val() + "')").parent("tr").show();
+    }
+    else{
+        jQuery("#tabla tbody>tr").show();
+    }
+});
+ 
+jQuery.extend(jQuery.expr[":"], 
+{
+    "contiene-palabra": function(elem, i, match, array) {
+        return (elem.textContent || elem.innerText || jQuery(elem).text() || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
 
     });
 });
@@ -14,7 +28,7 @@
 <table class="table table-light table-responsive-md" id="tabla">
 	<thead>
 		<tr><th class="titulo" colspan="6"><h3>Lista de Recursos</h3></th></tr>
-		<tr><th colspan="4" id="filtro"><div class="container-fluid">
+		<tr><th colspan="2" id="filtro">
 			<form id="buscar" method="post" action="<?php echo base_url()?>/application/controllers/usuario/buscar">
 				<div class="col-md-6">
                     <input type="text" class="form-control" id="busqueda"/>
@@ -40,7 +54,7 @@
 				<?php echo $r['descripcion']; ?>
 			</td>
 			<td>
-				<?php if($r['validado']==0){?><input type="checkbox" id="validado" name="validado"><?php }  ?>
+				<?php if($r['validado']==0){?><input type="checkbox" id="validado" name="validado"><?php }else{  ?><input type="checkbox" id="validado" name="validado" checked><?php }?>
 			</td>
 			<td>
 				<?php echo count($r['archivos']); ?>
--- application/views/usuario/index.php
+++ application/views/usuario/index.php
@@ -4,7 +4,21 @@
 <script type="text/javascript">
 	$(document).ready(function () {
     $("#busqueda").keyup(function () {
-        var filtro = $("#busqueda").val();
+		var filtro = $("#busqueda").val();
+		if( jQuery(this).val() != ""){
+        jQuery("#tabla tbody>tr").hide();
+        jQuery("#tabla td:contiene-palabra('" + jQuery(this).val() + "')").parent("tr").show();
+    }
+    else{
+        jQuery("#tabla tbody>tr").show();
+    }
+});
+ 
+jQuery.extend(jQuery.expr[":"], 
+{
+    "contiene-palabra": function(elem, i, match, array) {
+        return (elem.textContent || elem.innerText || jQuery(elem).text() || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
 
     });
 });
@@ -13,7 +27,7 @@
 <table class="table table-light table-responsive-md" id="tabla">
 	<thead>
 		<tr><th class="titulo" colspan="6"><h3>Lista de Usuarios</h3></th></tr>
-		<tr><th colspan="4" id="filtro"><div class="container-fluid">
+		<tr><th colspan="4" id="filtro">
 			<form id="buscar" method="post" action="<?php echo base_url()?>/application/controllers/usuario/buscar">
 				<div class="col-md-6">
                     <input type="text" class="form-control" id="busqueda"/>
