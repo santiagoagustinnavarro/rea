05e4a71233cafc0e4ea57954d975c9b8d6d86112
snavarro017 <santiagonavarro@outlook.com.ar>
2018-12-02 18:27:18 -0300

    seccion de comentario testeado con id recurso 1

--- application/controllers/Comentario.php
+++ application/controllers/Comentario.php
@@ -1,7 +1,39 @@
 <?php
-class Comentario extends CI_Controller{
-    
+class Comentario extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library("session");
+    }
+
+    public function generarComentario($idRecurso="")
+    {
+        if ($this->session->iniciada) {
+            $usuario=$this->session->nombreUsuario;
+            if ($this->input->method()==="post") {
+                if (!$comentario=$this->input->post("descripcion")) {
+                    $this->load->view("header", ["title"=>"Falta descripcion"]);
+                    $this->load->view("comentario/generarComentario");
+                    $this->load->view("footer");
+
+                }else{
+                    $this->load->model("Comentario_model");
+                    $this->Comentario_model->add_comentario(["nombreUsuario"=>$usuario,"descripcion"=>$comentario,"idRecurso"=>$idRecurso]);
+                    $this->load->view("header", ["title"=>"Comentario agregado"]);
+                    $this->load->view("comentario/generarComentario");
+                    $this->load->view("footer");
+
+                }
+            }
+            else{
+                
+                $this->load->view("header", ["title"=>"Agregar comentario"]);
+                $this->load->view("comentario/generarComentario");
+                $this->load->view("footer");
+
+            }
+        }
+    }
 }
-
-
 ?>
---
+++ application/views/comentario/generarComentario.php
@@ -0,0 +1,13 @@
+<?php
+
+
+echo form_open("comentario/generarComentario/1",["method"=>"post"]);
+?>
+<div class="form-group">
+<label for="descripcion">Ingrese su comentario</label>
+<textarea type="textarea" class="form-control offset-lg-4 col-lg-5" name="descripcion" id="descripcion"></textarea>
+</div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+<?php
+ echo form_close();
+?>
