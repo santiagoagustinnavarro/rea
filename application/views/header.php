f5eb7c148c2933f2054da70d7d6ef41348ff2c73
snavarro017 <santiagonavarro@outlook.com.ar>
2018-12-06 10:54:09 -0300

    header en recurso y nueva BD

--- application/views/header.php
+++ application/views/header.php
@@ -151,7 +151,7 @@
 		<?php
 			} elseif ($this->session->rol == 'administrador de recursos') {
     ?>
-		<nav class="navbar navbar-static-top">
+	<nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
@@ -172,7 +172,70 @@
               </ul>
             </li>
           </ul>
-        </div> <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.navbar-collapse -->
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+            <!-- Messages: style can be found in dropdown.less-->
+            <li class="dropdown messages-menu">
+              <!-- Menu toggle button -->
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <i class="fa fa-envelope-o"></i>
+                <span class="label label-success">4</span>
+              </a>
+              <ul class="dropdown-menu">
+                <li class="header">You have 4 messages</li>
+                <li>
+                  <!-- inner menu: contains the messages -->
+                  <ul class="menu">
+                    <li><!-- start message -->
+                      <a href="#">
+                        <div class="pull-left">
+                          <!-- User Image -->
+                          <img src="<?php echo base_url();?>assets/estilo/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        </div>
+                        <!-- Message title and timestamp -->
+                        <h4>
+                          Support Team
+                          <small><i class="fa fa-clock-o"></i> 5 mins</small>
+                        </h4>
+                        <!-- The message -->
+                        <p>Why not buy a new awesome theme?</p>
+                      </a>
+                    </li>
+                    <!-- end message -->
+                  </ul>
+                  <!-- /.menu -->
+                </li>
+                <li class="footer"><a href="#">See All Messages</a></li>
+              </ul>
+            </li>
+            <!-- /.messages-menu -->
+
+            <!-- Notifications Menu -->
+            <li class="dropdown notifications-menu">
+              <!-- Menu toggle button -->
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <i class="fa fa-bell-o"></i>
+                <span class="label label-warning">10</span>
+              </a>
+              <ul class="dropdown-menu">
+                <li class="header">You have 10 notifications</li>
+                <li>
+                  <!-- Inner Menu: contains the notifications -->
+                  <ul class="menu">
+                    <li><!-- start notification -->
+                      <a href="#">
+                        <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                      </a>
+                    </li>
+                    <!-- end notification -->
+                  </ul>
+                </li>
+                <li class="footer"><a href="#">View all</a></li>
+              </ul>
+            </li>
             <!-- User Account Menu -->
             <li class="dropdown user user-menu">
               <!-- Menu Toggle Button -->
@@ -199,12 +262,12 @@
                     <a href="<?php echo base_url(); ?>login/cerrarSession" class="btn btn-default btn-flat">Cerrar Sesion</a>
                   </div>
                 </li>
-              
+              </ul>
             </li>
-          </ul>
+      </ul>
         </div>
         <!-- /.navbar-custom-menu -->
-   
+      </div>
       <!-- /.container-fluid -->
     </nav>
 		<?php
@@ -323,7 +386,7 @@
                 </li>
               </ul>
             </li>
-          
+      </ul>
         </div>
         <!-- /.navbar-custom-menu -->
       </div>
