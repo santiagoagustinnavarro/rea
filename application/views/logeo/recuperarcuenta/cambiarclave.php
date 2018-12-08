b81f50705eee942c4c53acd0d5a6867b93092da9
ElyB06 <ely-06nqn@hotmail.com>
2018-12-08 17:21:11 -0300

    vistas de cambiar y restablecer clave eliminar cuenta

--- application/views/logeo/recuperarcuenta/cambiarclave.php
+++ application/views/logeo/recuperarcuenta/cambiarclave.php
@@ -1,30 +1,29 @@
-
-<div class="container py-5">
-	<div class="col-md-6 mx-auto">
-		<div class="container" id="transparencia">
-		<?php
-      
-            ?> 
-  			<form id="formulario" method="post">
-			<?php 
-			if(isset($mensaje)){ 
-			echo $mensaje;
-			}
-			?> 
-			  <h1>Reestablecer Contraseña</h1>
-                </br>
-  		  		<div class="form-group">
-					<label class="label" for="clave"><i class="fa fa-lock"></i> Contraseña</label>
-      				<input type="password" class="form-control" id="clave" placeholder="Ingrese contraseña" name="clave" minlength="8" maxlength="15" required>
-				</div>
-				<div class="form-group">
-					<label class="label" for="clave2"><i class="fa fa-lock"></i> Confirmar Contraseña</label>
-      				<input type="password" class="form-control" id="clave2" placeholder="Reingrese contraseña" name="clave2" minlength="8" maxlength="15" required>
-				</div>
-				<div class="form-group" id="boton">
-					<button type="submit" class="btn btn-success">Enviar</button>
-				</div>
-			</form>
-		</div> <!-- Cierre del container -->
-	</div> <!-- Cierre de la clase col -->
+<div class="container">
+	<br/>
+	<div class="col-md-offset-3 col-md-6">
+		<div class="box box-info" id="reestablecerClave">
+			<div class="register-box-body">
+  				<form id="formulario" method="post">
+					<?php 
+					if(isset($mensaje)){ 
+						echo $mensaje;
+					}
+					?> 
+					<h1>Reestablecer Contraseña</h1>
+					<br/>
+  		  			<div class="form-group has-feedback">
+      					<input type="password" class="form-control" id="clave" placeholder="Ingrese contraseña" name="clave" minlength="8" maxlength="15" required>
+						<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+					</div>
+					<div class="form-group has-feedback">
+      					<input type="password" class="form-control" id="clave2" placeholder="Confirmar contraseña" name="clave2" minlength="8" maxlength="15" required>
+						<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+					</div>
+					<div class="form-group has-feedback" id="boton">
+						<button type="submit" class="btn btn-success">Enviar</button>
+					</div>
+				</form>
+			</div> <!-- Cierre del register-box-body -->
+		</div> <!-- Cierre de la clase box box-info -->
+	</div> <!-- Cierre de la clase box box-info -->	
 </div> <!-- Cierre del container final -->
--- application/views/usuario/eliminarCuenta.php
+++ application/views/usuario/eliminarCuenta.php
@@ -2,25 +2,25 @@
 	
     if ($this->session->iniciada) {
 ?>
-<div class="container py-5">
-	<div class="col-md-6 mx-auto">
-		<div class="container-fluid">
-			<?php echo form_open('usuario/eliminarCuenta',array("class"=>"form")); ?>
-			<div class="alert alert-danger" id="eliminar">
-				<h2><b> Eliminar Cuenta</b></h2>
-				<h4>Esta seguro que desea eliminar su cuenta</h4></br>
-				<div id="boton">
-					<a href="<?php echo base_url()?>usuario/eliminarCuenta/<?php echo $_SESSION["nombreUsuario"];?>">
-						<button type="button" name="si" id="si" class="btn btn-success">Si</button>
-					</a>
-					<a href="<?php echo base_url()?>inicio">
-						<button type="button" name="no" id="no" class="btn btn-danger">No</button>
-					</a>
-				</div> <!-- Cierre del clase id boton -->
-			</div> <!-- Cierre del alert -->
+<div class="container">
+	<br/><br/>
+	<div class="col-md-offset-3 col-md-6">
+		<?php echo form_open('usuario/eliminarCuenta',array("class"=>"form")); ?>
+		<div class="box box-danger" id="eliminar">
+			<h2><b> Eliminar Cuenta</b></h2>
+			<h4>Esta seguro que desea eliminar su cuenta</h4><br/>
+			<div>
+				<a href="<?php echo base_url()?>usuario/eliminarCuenta/<?php echo $_SESSION["nombreUsuario"];?>">
+					<button type="button" name="si" id="si" class="btn btn-success">Si</button>
+				</a>
+				<a href="<?php echo base_url()?>inicio">
+					<button type="button" name="no" id="no" class="btn btn-danger">No</button>
+				</a>
+			</div> <!-- Cierre del clase id boton -->
+			<br/>
+		</div> <!-- Cierre del box -->
 			<?php echo form_close(); ?>
-		</div> <!-- Cierre del container-fluid -->
-	</div> <!-- Cierre de la clases col-md-6 -->
+	</div> <!-- Cierre del col -->
 </div> <!-- Cierre del container final -->
 <?php
 	}
