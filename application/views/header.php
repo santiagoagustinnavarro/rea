228c0c1aa46cf51b0e681812839d08ca6d903132
santiagoagustinnavarro <tu-correo-electronico@example.com>
2023-09-20 00:19:31 -0300

    Remove unnecessary IonIcons

--- application/views/header.php
+++ application/views/header.php
@@ -15,16 +15,13 @@
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/bootstrap/dist/css/bootstrap.min.css">
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/font-awesome/css/font-awesome.min.css">
-	<!-- Ionicons -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/Ionicons/css/ionicons.min.css">
 	<!-- DataTables -->
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/datatables.net-bs/css/dataTables.bootstrap.min.css">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/dist/css/AdminLTE.min.css">
 	<!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/dist/css/skins/_all-skins.min.css">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/dist/css/skins/skin-blue.min.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/dist/css/skins/skin-blue.min.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/estilo/css/estilo.css">
 	<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/jquery-ui/jquery-ui.min.js"></script>
